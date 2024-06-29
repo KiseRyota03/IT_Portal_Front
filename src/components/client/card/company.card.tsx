591b0d5993bdccf7d309bbf7779b89c7e687795b
haohandaitruongphu <hiephomhinh55@gmail.com>
2024-06-29 10:21:34 +0700

    Update .gitignore to exclude .env

--- src/components/client/card/company.card.tsx
+++ src/components/client/card/company.card.tsx
@@ -89,6 +89,7 @@ const CompanyCard = (props: IProps) => {
                                             <div className={styles["card-customize"]} >
                                                 <img
                                                     alt="example"
+                                                    style={{ height: 250, width: 200 }}
                                                     src={`${import.meta.env.VITE_BACKEND_URL}/images/company/${item?.logo}`}
                                                 />
                                             </div>
